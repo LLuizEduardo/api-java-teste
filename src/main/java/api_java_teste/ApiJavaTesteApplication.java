1136177298024bb9637abffbbbcc3265ba06f3de
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2024-06-25 10:58:52 -0300

    first commit

---
+++ src/main/java/api_java_teste/ApiJavaTesteApplication.java
@@ -0,0 +1,13 @@
+package api_java_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api_java_teste/ApiJavaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package api_java_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
